55e83ff0020dd910da12f044f0f459732c79475a
prefixsum <prefixsum@outlook.com>
2024-06-07 21:46:16 +0100

    Implement parse expression from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-mod parser;
-
-use parser::Token;
+mod grammar;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a regular expression:");
+    let mut expression = String::new();
+    std::io::stdin()
+        .read_line(&mut expression)
+        .expect("Failed to read line");
+
+    let expression = expression.trim();
+    let expression_tree = grammar::Expression::new(expression);
+
+    println!("Parsed Expression: {:?}", expression_tree);
 }
